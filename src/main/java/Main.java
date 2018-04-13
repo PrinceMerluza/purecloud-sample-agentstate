9869d469279eae7e72fa7fa3980d52e227754da7
hero2bash <zero.bash@gmail.com>
2018-04-13 09:23:49 +0800

    Make code more compact

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,22 +34,28 @@ public class Main {
         // Instantiate APIs
         GroupsApi groupsApi = new GroupsApi();
 
-        // Get the group members and subscribe to presence,routing status topics..
+        // Get the group using the group name
         Group theGroup = getGroup(groupName, groupsApi);
+
+        // Get the members of the group
         List<User> users = getGroupMembers(theGroup, groupsApi);
 
+        // Build the notification handler
         NotificationHandler notificationHandler = NotificationHandler.Builder.standard()
                 .withWebSocketListener(new MyWebSocketListener())
                 .withNotificationListener(new ChannelMetadataListener())
                 .withAutoConnect(false)
                 .build();
 
+        // Subscribe the notification handler to the presence of group members
         subscribeToUserGroupPresence(users, notificationHandler);
+
+        // Display to confirm websocket client is listening
         System.out.println("Listening...");
     }
 
     /**
-     * Subscribe the handler to the users' presence and routing statuses.
+     * Subscribe the notification handler to the users' presence and routing statuses.
      * @param usersList contains the list of users to subscribe to
      * @param handler 	notificationhandler reference
      */
@@ -58,7 +64,6 @@ public class Main {
         for(User user : usersList) {
             handler.addSubscription(new UserPresenceListener(user.getId(), user.getName()));
             handler.addSubscription(new UserRoutingStatusListener(user.getId(), user.getName()));
-            System.out.println("Subscribed to: " + user.getName());
         }
     }
 
@@ -66,22 +71,21 @@ public class Main {
      * Get members of a group.
      * @param group	PureCloud group to get all members from
      * @param api	GroupsApi for calling api functions
-     * @return		list of Users from the group
+     * @return		List of Users from the group
      */
     private static List<User> getGroupMembers(Group group, GroupsApi api) throws ApiException, IOException{
+        // The list that will contain the group members
         List<User> members = new ArrayList<User>();
-        int pageSize = 50; //arbitrary number
-        int pageCount = (group.getMemberCount().intValue()/pageSize) + 1;
 
-        try {
-            // Synchronous calls to get group members and fill up the List
-            for(int i = 1;i <= pageCount;i++) {
-                UserEntityListing result = api.getGroupMembers(group.getId(), pageSize, i, "ASC", null);
-                members.addAll(result.getEntities());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        // Get the number of pages of the group and loop through all them to get all members
+        for(int i = 1;i <= (group.getMemberCount().intValue()/25) + 1;i++) {
+            // API Call to get current page and members
+            UserEntityListing result = api.getGroupMembers(group.getId(),25, i, "ASC", null);
+
+            // Add the members of the page to the List
+            members.addAll(result.getEntities());
         }
+
         return members;
     }
 
@@ -92,33 +96,18 @@ public class Main {
      * @return		First PureCloud Group that is found.
      */
     private static Group getGroup(String name, GroupsApi api) throws ApiException, IOException{
-        Group result = null;
-
         // Search criteria is group name with exact value.
-        GroupSearchCriteria criteria = new GroupSearchCriteria();
-        criteria.setValue(name);
-        criteria.setOperator(GroupSearchCriteria.OperatorEnum.AND);
-        criteria.setFields(new ArrayList<String>(Arrays.asList(new String[] {"name", "id"})));
-        criteria.setType(TypeEnum.EXACT);
+        GroupSearchCriteria criteria = new GroupSearchCriteria()
+                                           .value(name)
+                                           .operator(GroupSearchCriteria.OperatorEnum.AND)
+                                           .fields(new ArrayList<>(Arrays.asList("name")))
+                                           .type(TypeEnum.EXACT);
 
-        // Build query
-        List<GroupSearchCriteria> query = new ArrayList<GroupSearchCriteria>();
-        query.add(criteria);
+        // Build list of Group Search Criteria
+        GroupSearchRequest request = new GroupSearchRequest().query(new ArrayList<>(Arrays.asList(criteria)));
 
-        GroupSearchRequest request = new GroupSearchRequest();
-        request.setQuery(query);
-
-        try {
-            // Call Groups api for result. Should only be 1.
-            GroupsSearchResponse response = api.postGroupsSearch(request);
-            if(response.getTotal() > 1) {
-                System.out.println("Warning: More than 1 group was found. Getting first result.");
-            }
-            result = response.getResults().get(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+        // Return the group that was found. Should only be 1, if not, get the first one.
+        return api.postGroupsSearch(request).getResults().get(0);
     }
 
     /**
@@ -127,44 +116,35 @@ public class Main {
      * @param clientSecret  OAuth client secret
      * @return String		access token
      */
-    private static String getToken(String clientId, String clientSecret)throws ApiException, IOException {
+    private static String getToken(String clientId, String clientSecret)throws IOException {
         String token = "";
 
         // Token Request info + encoded client credentials
         String url = "https://login.mypurecloud.com/oauth/token";
         String credentials = clientId + ":" + clientSecret;
-        String encodedCredentials = Base64.getEncoder()
-                                    .encodeToString(credentials.getBytes());
+        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
 
-        //System.out.println(encodedCredentials);
-        try {
-            // Build HTTP Request Information
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setRequestProperty("Authorization", "Basic " + encodedCredentials);
-            connection.setDoOutput(true);
+        // Build HTTP Request Information
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Authorization", "Basic " + encodedCredentials);
+        connection.setDoOutput(true);
 
-            // HTTP Request Body
-            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-            wr.writeBytes("grant_type=client_credentials");
-            wr.close();
+        // HTTP Request Body
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.writeBytes("grant_type=client_credentials");
+        wr.close();
 
-            // HTTP Response with token
-            if(connection.getResponseCode() == 200) {
-                InputStream response = connection.getInputStream();
-                String responseString =  Helper.convertStreamToString(response);
-                // Hacky-way of extracting token from response string
-                // so we don't have to add external library for JSON parsing
-                token = responseString.substring(responseString.indexOf(':')+2,
-                        responseString.indexOf(',')-1).trim();
-            }else {
-                System.out.println("Token not acquired.");
-            }
+        // Check if HTTP Response is successful
+        if(connection.getResponseCode() == 200) {
+            InputStream response = connection.getInputStream();
+            String responseString =  Helper.convertStreamToString(response);
 
-        } catch (Exception e) {
-            System.out.println("Error with HTTP connection");;
-            e.printStackTrace();
+            // Hacky-way of extracting token from response string
+            // so we don't have to add external library for JSON parsing
+            token = responseString.substring(responseString.indexOf(':')+2,
+                    responseString.indexOf(',')-1).trim();
         }
 
         return token;
